16badba5cd211bda3a4f139b67abd2c1e9b7b92e
tshi206 <tshi206@aucklanduni.ac.nz>
2018-07-26 02:08:33 +1200

    up to tut 3

--- solidity_tuts/contracts/tut2.sol
+++ solidity_tuts/contracts/tut2.sol
@@ -1,11 +1,23 @@
 pragma solidity ^0.4.24;
 
+// TODO: TUT UP TO 3, NEXT START: 4
+
 interface Regulator {
     function checkValue(uint amount) external returns (bool);
     function loan() external  returns (bool);
 }
 
 contract Bank is Regulator {
+
+    address private owner;
+
+    modifier ownerFunc {
+        require(owner == msg.sender);
+        _; // placeholder for our functions being modifier by this ownerFunc modifier
+        // this is basically saying the modifier's callback ( require(...) in this case ) will always be fired before
+        // executing the decorated function.
+        // In another word, make sure the modifier is always checked first before our decorated functions ever execute
+    }
     
     // internal => protected
     uint internal someInt;
@@ -14,13 +26,14 @@ contract Bank is Regulator {
     
     constructor(uint amount) public {
         value = amount;
+        owner = msg.sender;
     }
     
-    function deposit(uint amount) public {
+    function deposit(uint amount) internal ownerFunc {
         value += amount;
     }
     
-    function withdraw(uint amount) public {
+    function withdraw(uint amount) internal ownerFunc {
         if (checkValue(amount)) {
             value -= amount;
         }
@@ -74,4 +87,24 @@ contract MyFirstContract is Bank(10) {
         return true;
     }
     
+}
+
+contract TestThrows {
+
+    function testAssert() {
+        assert(1 == 2);
+    }
+
+    function testRequire() {
+        require(2 == 1);
+    }
+
+    function testRevert() {
+        revert();
+    }
+
+    function testThrow() {
+        throw;
+    }
+
 }
